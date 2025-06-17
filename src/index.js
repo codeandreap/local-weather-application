f7bc66241854e3689ce19cf7afcc09de0da68d02
codeandreap <anpash72@gmail.com>
2025-06-17 19:26:24 +0100

    Added more weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@ function updateWeatherTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-app-city");
+  let weatherConditionElement = document.querySelector(
+    "#current-weather-condition"
+  );
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let currentTimeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  currentTimeElement.innerHTML = formatDate(date);
+  weatherConditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
